e5af7c7f172e676563e39b46a9905d8bd4342b05
Roel <rgburght@gmail.com>
2017-03-16 19:19:24 +0100

    Merge branch 'master' of https://github.com/RoelvdBurght/Amstelhaegee
    # Conflicts:
    #	Class.py

---
+++ Canvas.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import tkinter
+import Class
+
+
+canvas = Canvas(width=600, height=600, bg='white')
+canvas.pack(expand=YES, fil=BOTH)
+
+canvas.create_rectangle(200, 200, 300, 300, width=5, fill='red')
+
+
+def addMaison(x, y):
+    newMaison = canvas.create_rectangle(x, y, (x + 11), (y + 10.5))
+
+def addBungalow(x,y)
+addMaison(2,6)
+mainloop()
